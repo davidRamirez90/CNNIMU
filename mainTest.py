0c7ba1ed9426c5a4d9ecd0862db46b7ab8be81f6
dramirez.c90 <dramirez.c90@gmail.com>
2019-09-03 01:59:30 +0200

    [dbg] maintest

--- mainTest.py
+++ mainTest.py
@@ -159,6 +159,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # name = getName(args.type)
     configs = init(args)
+    print(configs)
 
     with open('testResults.csv', mode='w') as csv_file:
         fields = ['win_len','win_step','name', 'accuracy', 'loss', 'f1', 'acc_[0]', 'acc_[1]', 'acc_[2]', 'acc_[3]', 'acc_[4]', 'acc_[5]', 'acc_[6]']
