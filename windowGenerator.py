a8f6ca0c03756e317863933633f3726936a8fdd9
dramirez.c90 <dramirez.c90@gmail.com>
2019-09-24 18:53:09 +0200

    solved??? no

--- windowGenerator.py
+++ windowGenerator.py
@@ -251,7 +251,8 @@ class WindowGenerator:
                         skdata = self.read_data(file)
                         mkdata = self.read_data_markers(markerfile).astype('float64')
                         labels = skdata[:mkdata.shape[0],0].reshape((-1,1))
-
+                        if file == '/vol/corpora/har/DFG_Project/2019/MoCap/recordings_2019/14_i219/S01_P03_R06_A08_N01_norm.csv':
+                            pdb.set_trace()
                         nanfilter = np.isnan(mkdata).any(axis=1)
                         labels = labels[~nanfilter]
                         mkdata = mkdata[~nanfilter]
