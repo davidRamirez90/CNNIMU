9c6a75736a62cfe8f1d7a8bf54811d5c3aa75109
dramirez.c90 <dramirez.c90@gmail.com>
2019-10-17 14:47:07 +0200

    cnn_base - ONLY TRAINING DATA GENERATED / wingen main can generate all datasets first

--- windowGenerator.py
+++ windowGenerator.py
@@ -511,11 +511,13 @@ class WindowGenerator:
 if __name__ == '__main__':
 
     # TYPE 0
+    print("Creating Skeletons")
     wg1 = WindowGenerator(100, 5, 138)
     wg1.run()
-
-    wg2 = WindowGenerator(100, 5)
+    print("Creating Markers")
+    wg2 = WindowGenerator(100, 5, 38)
     wg2.runMarkers()
+    print("Creating Accel")
     wg2.runDerivation()
     # wg1 = WindowGenerator(300, 15)
     # wg1.runMarkers()
