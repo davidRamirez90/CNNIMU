79ed8d578bc02b5cbf3d43aa58140305eae2a431
dramirez.c90 <dramirez.c90@gmail.com>
2019-10-18 02:25:36 +0200

    [stable] cnn_base_w with new testing data

--- datasetAnalyzer.py
+++ datasetAnalyzer.py
@@ -45,15 +45,55 @@ class Analyzer:
         print('Results for {}'.format(name))
         print(self.sum)
 
+class StatAnalyzer:
+
+    def __init__(self):
+        self.list = [0,1,2,3,4,5,6]
+
+    def read_data(self, path):
+        data = pd.read_csv(path)
+        data = data.iloc[:, 1]
+        data = self.remove_class(data, 7)
+
+        if (data.size > 0):
+            a=0
+
+    def avg(self, x):
+        print(x)
+        print('log')
+
+    def run(self):
+        keys = set(self.list)
+        class_dict = dict((x, list()) for x in keys)
+        print(class_dict)
+        prev = 999
+        count = 0
+        for f_class in self.list:
+            if f_class == prev:
+                count+=1
+            else:
+                if prev != 999:
+                    class_dict[prev].append(count)
+                count = 1
+                prev = f_class
+        class_dict[prev].append(count)
+        print(class_dict)
+        stats = dict((x, self.avg(class_dict[x])) for x in class_dict.keys())
+        return class_dict
 
 
 if __name__ == '__main__':
-    an = Analyzer()
+    # an = Analyzer()
+    #
+    # an.run({'name': 'Training', 'data_sets': ['P01', 'P02', 'P03']})
+    # an.run({'name': 'Validating', 'data_sets': ['P04']})
+    # an.run({'name': 'Testing', 'data_sets': ['P05', 'P06']})
 
-    an.run({'name': 'Training', 'data_sets': ['P01', 'P02', 'P03']})
-    an.run({'name': 'Validating', 'data_sets': ['P04']})
-    an.run({'name': 'Testing', 'data_sets': ['P05', 'P06']})
 
+    a = StatAnalyzer()
+
+    stats = a.run()
+    print(stats)
 
 
     # /Users/dramirez.c90/Desktop/dataset/m/Subject01_01.csv
