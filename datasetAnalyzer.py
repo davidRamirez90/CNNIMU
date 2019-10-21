43beca3f5732262906fffb3ab1df6568ab4e6a17
dramirez.c90 <dramirez.c90@gmail.com>
2019-10-21 23:44:27 +0200

    [stable] dataset statistics P01 - P04

--- datasetAnalyzer.py
+++ datasetAnalyzer.py
@@ -5,14 +5,15 @@ import torch
 import glob
 import env
 import tqdm
+import csv
 import pdb
 
 
 class Analyzer:
 
     def __init__(self):
-        self.data_folder = "/Users/dramirez.c90/Desktop/dataset/m/"
-        # self.data_folder = env.dataset_url
+        # self.data_folder = "/Users/dramirez.c90/Desktop/dataset/m/"
+        self.data_folder = env.dataset_url
         self.device = torch.device(
             'cuda:0' if torch.cuda.is_available() else "cpu")
         self.sum = torch.zeros(7).type(torch.IntTensor)
@@ -48,38 +49,50 @@ class Analyzer:
 class StatAnalyzer:
 
     def __init__(self):
-        self.list = [0,1,2,3,4,5,6]
+        self.data_folder = env.dataset_url
+        self.keys = [0, 1, 2, 3, 4, 5, 6, 7]
+        self.class_dict = dict((x, list()) for x in self.keys)
+
+    def remove_class(self, data, cl):
+        indices = data != cl
+        return data[indices]
 
     def read_data(self, path):
         data = pd.read_csv(path)
         data = data.iloc[:, 1]
-        data = self.remove_class(data, 7)
-
-        if (data.size > 0):
-            a=0
-
-    def avg(self, x):
-        print(x)
-        print('log')
-
-    def run(self):
-        keys = set(self.list)
-        class_dict = dict((x, list()) for x in keys)
-        print(class_dict)
+        # data = self.remove_class(data, 7)
         prev = 999
         count = 0
-        for f_class in self.list:
-            if f_class == prev:
-                count+=1
-            else:
-                if prev != 999:
-                    class_dict[prev].append(count)
-                count = 1
-                prev = f_class
-        class_dict[prev].append(count)
-        print(class_dict)
-        stats = dict((x, self.avg(class_dict[x])) for x in class_dict.keys())
-        return class_dict
+        if (data.size > 0):
+            for f_class in data:
+                if f_class == prev:
+                    count+=1
+                else:
+                    if prev != 999:
+                        self.class_dict[prev].append(count)
+                    count = 1
+                    prev = f_class
+            self.class_dict[prev].append(count)
+            print(self.class_dict)
+
+
+    def run(self, input):
+
+
+        data_dict = input['data_sets']
+        name = input['name']
+        for dir in data_dict:
+            files = glob.glob(self.data_folder.format(dir))
+            for i, f in enumerate(tqdm.tqdm(files)):
+                print('Running for {}'.format(f))
+                self.read_data(f)
+        res_frame = pd.concat([pd.DataFrame(x) for x in self.class_dict.values()], axis=1)
+
+        export = res_frame.to_csv('samplefile.csv', index=None, header=True)
+        print('Results for {}'.format(name))
+        print(self.sum)
+        return True
+
 
 
 if __name__ == '__main__':
@@ -87,16 +100,14 @@ if __name__ == '__main__':
     #
     # an.run({'name': 'Training', 'data_sets': ['P01', 'P02', 'P03']})
     # an.run({'name': 'Validating', 'data_sets': ['P04']})
-    # an.run({'name': 'Testing', 'data_sets': ['P05', 'P06']})
-
-    path = "/Users/dramirez.c90/Desktop/dataset/m/Subject01_01.csv"
-    data = pd.read_csv(path)
+    # # an.run({'name': 'Testing', 'data_sets': ['P05', 'P06']})
+    #
+    # path = "/Users/dramirez.c90/Desktop/dataset/m/Subject01_01.csv"
+    # data = pd.read_csv(path)
 
 
     a = StatAnalyzer()
 
-    stats = a.run()
-    print(stats)
+    stats = a.run({'name': 'Training', 'data_sets': ['P01', 'P02', 'P03', 'P04']})
 
 
-    # /Users/dramirez.c90/Desktop/dataset/m/Subject01_01.csv
